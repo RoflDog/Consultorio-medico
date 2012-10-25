0093107e379d480fe258903b99d6a4049befc874
imalac <el_gran_caifan@hotmail.com>
2012-10-25 00:05:53 -0500

    Agregado controladores y modelo de usuarios

---
+++ consultorio/models/UsersModel.js
@@ -0,0 +1,23 @@
+// The Users Model
+
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,ObjectId = Schema.ObjectId;
+
+var userSchema = new Schema({
+	_id : ObjectId,
+	username : String,
+	password : String,
+	salt : String,
+	active : Boolean,
+	firstName : String,
+	lastName : String,
+	birthDate : Date,
+	email : String,
+	phone : [{type : String, format : "phone"}],
+	address : String,
+	roles : [{type : String, enum: ['doctor', 'secretary', 'administrator']}],
+	schedule : { start: {type : "number"}, end: {type : "number"} }
+});
+
+module.exports = mongoose.model('User', userSchema);
