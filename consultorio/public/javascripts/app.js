d42835f44ad52f5978e06d292203f8cc9e1b88d6
RoflDog <rofldogstudios@groups.live.com>
2012-11-06 22:59:49 -0600

    Modificado un poco las vistas

--- consultorio/public/javascripts/app.js
+++ consultorio/public/javascripts/app.js
@@ -9,6 +9,14 @@ angular.module('Consultorio',[]).
             templateUrl:'partials/addUser',
             controller: AddUserCtrl
         }).
+        when('/indexUser',{
+            templateUrl:'partials/indexUser',
+            //controller: AddUserCtrl
+        }).
+        when('/indexPatient',{
+            templateUrl:'partials/indexPatient',
+            //controller: AddUserCtrl
+        }).
         otherwise({
             redirectTo:'/'
         })
