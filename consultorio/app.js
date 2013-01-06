40d45249d9634d4c67373a8d377be8d19f3cfa6f
Daniel Guerra <daniel_guerra@outlook.com>
2013-01-05 18:05:02 -0600

    Ruta arreglada para api citas

--- consultorio/app.js
+++ consultorio/app.js
@@ -73,7 +73,7 @@ app.get('/api/charges/:id', payments.getCharge);
 app.get('/api/session', session.getSessionInformation);
 
 //API for appointments
-app.get('/api/appointment', appointment.list);
+app.get('/api/appointments', appointment.list);
 app.get('/api/appointment/:id', appointment.get);
 app.post('/api/appointment', appointment.add);
 app.put('/api/appointment/:id', appointment.update);
